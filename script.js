948d74d5f08febccd6045756d6636e39c4705ddd
Bl4ck-lion <67418383+Bl4ck-lion@users.noreply.github.com>
2024-09-04 11:13:28 +0700

    coba

--- script.js
+++ script.js
@@ -10,10 +10,7 @@ window.addEventListener("scroll", ()=> {
     let value = window.scrollY;
     moon.style.top = value * .9 + "px";
     text.style.top = 80 + value * -0.2 + '%';
-    train.style.left = value * 1.5 + "px";
 
-    desert_room.style.top = value * .3 + "px";
-    man.style.left = value * .6 + "px";
 
 })
 
@@ -35,6 +32,8 @@ let clacScrollValue = ()=>{
     });
     scrollProgress.style.background = 'conic.gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)';
 };
+
+
 
 window.onscroll = clacScrollValue;
 window.onload = clacScrollValue;
